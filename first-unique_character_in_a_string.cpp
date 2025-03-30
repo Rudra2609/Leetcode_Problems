c560158c9b769580366d9e2a6f250c17d7268327
Rudra2609 <rudratrivedi30303@gmail.com>
2025-03-30 21:20:57 +0530

    Update first-unique_character_in_a_string.cpp

--- first-unique_character_in_a_string.cpp
+++ first-unique_character_in_a_string.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int firstUniqChar(string s){
+        int ans,c=0;
+        unordered_map<char,int> freq;
 
+        for(int i=0;i<s.size();i++){
+            freq[s[i]]++;            
+        }
+
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]]==1){
+                ans=i;
+                c=1;
+                break;
+            }
+        }
+        if(c) return ans;
+        else return -1;
+    }
+};
