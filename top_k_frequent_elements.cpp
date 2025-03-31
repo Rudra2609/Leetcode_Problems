7302301a373218fa2d6dd1c9f58e9aea8268dfc3
Rudra2609 <rudratrivedi30303@gmail.com>
2025-03-31 15:04:31 +0530

    Update top_k_frequent_elements.cpp

--- top_k_frequent_elements.cpp
+++ top_k_frequent_elements.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int,greater<int>> freq;
+        vector<int> ans;
+        int a=0;
 
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+
+        priority_queue<pair<int,int>> pq;
+        for(auto& i: freq){
+            pq.push({i.second,i.first});
+        }
+
+        while (k-- && !pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
