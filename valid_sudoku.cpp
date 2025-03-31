e751cd3420c4d97f6fc51b62851768661c3a5299
Rudra2609 <rudratrivedi30303@gmail.com>
2025-03-31 15:18:55 +0530

    Update valid_sudoku.cpp

--- valid_sudoku.cpp
+++ valid_sudoku.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<int>> box(10, vector<int>(10, 0));
+
+        for(int i=0;i<9;i++){
+            vector<int> tp(10,0),tp1(10,0);
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                    if(tp[board[i][j]-'0']++) return false;
+                }
+                if(board[j][i]!='.'){
+                    if(tp1[board[j][i]-'0']++) return false;
+                }
+                if(board[i][j]!='.'){
+                    int ind=(i/3)*3+(j/3);
+                    if(box[ind][board[i][j]-'0']++) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
 
