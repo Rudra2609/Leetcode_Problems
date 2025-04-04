6ee4c19fb1120acbc0e3abc9fffc4677c6354268
Rudra2609 <rudratrivedi30303@gmail.com>
2025-04-04 12:01:40 +0530

    Update top_k_frequent_elements.cpp

--- top_k_frequent_elements.cpp
+++ top_k_frequent_elements.cpp
@@ -21,3 +21,31 @@ public:
         return ans;
     }
 };
+
+
+// another method
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int> freq;
+        multimap<int,int,greater<int>> tp;
+        vector<int> ans;
+        int a=0;
+
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+
+        for(auto i:freq){
+            tp.insert({i.second,i.first});
+        }
+
+        for(auto i:tp){
+            if(!k) break;
+            ans.push_back(i.second);
+            k--;
+        }
+        return ans;
+    }
+};
